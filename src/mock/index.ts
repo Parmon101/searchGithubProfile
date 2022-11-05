1f1ec2e998a5b0566b4e7caa71a72cba8fd884a4
Evgeny Belyakov <bfjeka101@gmail.com>
2022-11-05 12:47:16 +0300

    done

---
+++ src/mock/index.ts
@@ -0,0 +1,16 @@
+import { LocalGithubUser } from 'types';
+
+export const defaultUser: LocalGithubUser = {
+  login: 'Parmon101',
+  avatar: 'https://avatars.githubusercontent.com/u/48516892?v=4',
+  name: 'Evgeny',
+  company: '',
+  blog: '',
+  location: '',
+  bio: '🚀 Frontend Developer 🚀\r\nJavaScript, \r\nTypeScript, React.js, Vue.js, Redux, Mobx',
+  twitter: '',
+  repos: 25,
+  followers: 1,
+  following: 1,
+  created: '2019-03-13T11:39:51Z',
+};
